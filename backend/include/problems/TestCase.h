66e43b889f18617a64c607f2ed8c0d3aa6fd0d78
Anuj Negi <negianuj.0930@gmail.com>
2026-09-15 21:35:40 +0530

    Add files via upload

---
+++ backend/include/problems/TestCase.h
@@ -0,0 +1,19 @@
+#ifndef TEST_CASE_H
+
+#define TEST_CASE_H
+
+#include <string>
+
+struct TestCase {
+
+    int testCaseId;
+
+    std::string input;
+
+    std::string expectedOutput;
+
+    bool isSample;
+
+};
+
+#endif
