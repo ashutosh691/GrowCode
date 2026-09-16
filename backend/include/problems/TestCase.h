f25992439b7edd9c63f67ebf594a383cb5dae743
Anuj Negi <negianuj.0930@gmail.com>
2026-09-16 20:37:24 +0530

    fixed struct

--- backend/include/problems/TestCase.h
+++ backend/include/problems/TestCase.h
@@ -1,19 +1,13 @@
 #ifndef TEST_CASE_H
-
 #define TEST_CASE_H
 
 #include <string>
 
 struct TestCase {
-
     int testCaseId;
-
     std::string input;
-
     std::string expectedOutput;
-
     bool isSample;
-
 };
 
 #endif
